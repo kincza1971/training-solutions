2325376244a1a972e2c9429805aadbecf5cab654
Laci <kincza@gmail.com>
2020-11-03 09:32:50 +0100

    statements

---
+++ src/main/java/statements/StatementMain.java
@@ -0,0 +1,13 @@
+package statements;
+
+public class StatementMain {
+    public static void main(String[] args) {
+        int x = 5+6;
+        int y = 11-x;
+        int z = 8;
+        boolean b = x>z;
+        boolean c= b || (z>5);
+        z++;
+    }
+
+}
