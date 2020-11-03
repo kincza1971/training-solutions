9ccff992835f4f47965c04bc48f39f002f4d4315
Laci <kincza@gmail.com>
2020-11-03 09:46:25 +0100

    statements

---
+++ src/main/java/statements/Time.java
@@ -0,0 +1,28 @@
+package statements;
+
+public class Time {
+    int hours;
+    int minutes;
+    int seconds;
+
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+    public int getInMinutes() {
+        return (hours * 60) + minutes;
+    }
+    public int getInSeconds() {
+        return (hours * 3600) + (minutes * 60) + seconds;
+    }
+
+    public boolean isEearlierThan(Time aTime) {
+        return getInSeconds()>aTime.getInSeconds();
+    }
+
+    public String toString() {
+        return hours + ":" + minutes + ":" + seconds;
+    }
+
+}
