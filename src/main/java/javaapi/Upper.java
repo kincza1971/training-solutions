0bc0ca14af03f7b5bc746d71a76c0bbf86862eee
Laci <kincza@gmail.com>
2020-11-03 11:45:23 +0100

    statements

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,9 @@
+package javaapi;
+
+public class Upper {
+    public static void main(String[] args) {
+        String hello = "hello world";
+        hello = hello.toUpperCase();
+        System.out.println(hello);
+    }
+}
--- src/main/java/packages/main/MainProgram.java
+++ src/main/java/packages/main/MainProgram.java
@@ -4,7 +4,7 @@ import packages.greetings.Greeter;
 
 public class MainProgram {
     public static void main(String[] args) {
-        packages.greetings.Greeter greeter = new Greeter();
+        Greeter greeter = new Greeter();
         greeter.sayHello();
     }
 }
