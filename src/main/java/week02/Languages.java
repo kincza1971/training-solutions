b2279995b5e67f9975a436ac58902c9ecc072595
Laci <kincza@gmail.com>
2020-11-06 13:21:02 +0100

    debug2

--- src/main/java/week02/Languages.java
+++ src/main/java/week02/Languages.java
@@ -1,16 +1,17 @@
 package week02;
 
-import javax.swing.text.StyledEditorKit;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Languages {
-    List<String> prg = new ArrayList<>();
+    private List<String> prgs = new ArrayList<>();
+
     public static void main(String[] args) {
         Languages lang = new Languages();
-        lang.prg.add("Java");
-        lang.prg.add("Python");
-        lang.prg.add("JavaScript");
+
+        lang.prgs.add("Java");
+        lang.prgs.add("Python");
+        lang.prgs.add("JavaScript");
 
         for (String lg: lang.prg) {
             if (lg.length()>5) {
