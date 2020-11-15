0f46e1d65d3045470007737d4975897a2d718919
Laci <kincza@gmail.com>
2020-11-15 11:26:46 +0100

    filemanipulator

--- src/main/java/stringmethods/filename/FileNameManipulator.java
+++ src/main/java/stringmethods/filename/FileNameManipulator.java
@@ -1,26 +1,26 @@
-package strinmethods;
+package stringmethods.filename;
 
 public class FileNameManipulator {
 
     private void stringValidator (String fName) throws IllegalArgumentException{
         if (fName == null || fName.isBlank()) {
-            throw new IllegalArgumentException("Üres vagy null string nem lehet paraméter");
+            throw new IllegalArgumentException("Invalid argument!");
         }
     }
 
     public char findLastCharacter(String str) {
         stringValidator(str);
-        return str.charAt(str.length()-1);
+        return str.trim().charAt(str.trim().length()-1);
     }
 
 
     public String findFileExtension(String fileName) {
         stringValidator(fileName);
         int indexOfDot =fileName.lastIndexOf('.');
-        if (indexOfDot==-1 || indexOfDot == fileName.length()-1) {
-            throw new IllegalArgumentException("A parameter tartalmaz kiterjesztést");
+        if (indexOfDot<1 || indexOfDot == fileName.length()-1) {
+            throw new IllegalArgumentException("A parameter nem tartalmaz kiterjesztést");
         }
-        return fileName.substring(indexOfDot+1);
+        return fileName.substring(indexOfDot);
     }
 
 
@@ -30,8 +30,8 @@ public class FileNameManipulator {
         fileName = fileName.trim();
         ext = ext.trim();
         int indexOfDot = fileName.lastIndexOf('.');
-        if (indexOfDot==-1 || indexOfDot == fileName.length()-1) {
-            throw new IllegalArgumentException(" A fájlnak nincs kiterjesztése");
+        if (indexOfDot<1 || indexOfDot == fileName.length()-1) {
+            throw new IllegalArgumentException("A fájlnak nincs kiterjesztése");
         }
         return(fileName.substring(indexOfDot+1).equals(ext));
     }
@@ -39,17 +39,17 @@ public class FileNameManipulator {
     public boolean compareFilesByName(String searchedFileName, String actualFileName) {
         stringValidator(searchedFileName);
         stringValidator(actualFileName);
-        return searchedFileName.trim().equals(actualFileName.trim());
+        return searchedFileName.trim().toLowerCase().equals(actualFileName.trim().toLowerCase());
     }
 
     public String changeExtensionToLowerCase(String fileName) throws IllegalArgumentException {
         stringValidator(fileName);
         fileName = fileName.trim();
         int indexOfDot = fileName.lastIndexOf('.');
-        if (indexOfDot==-1 || indexOfDot == fileName.length()-1) {
-            throw new IllegalArgumentException(" A fájlnak nincs kiterjesztése");
+        if (indexOfDot<1 || indexOfDot == fileName.length()-1) {
+            throw new IllegalArgumentException("Invalid argument!");
         } else {
-            return fileName.substring(0,indexOfDot) + fileName.substring(indexOfDot+1).toLowerCase();
+            return fileName.substring(0,indexOfDot) + fileName.substring(indexOfDot).toLowerCase();
         }
     }
 
