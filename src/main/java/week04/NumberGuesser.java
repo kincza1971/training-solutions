3942df3dfc2e0f90e4426c0074a8d1277661ecab
Laci <kincza@gmail.com>
2020-11-18 13:26:53 +0100

    guess

--- src/main/java/week04/NumberGuesser.java
+++ src/main/java/week04/NumberGuesser.java
@@ -11,16 +11,21 @@ public class NumberGuesser {
         int num = rnd.nextInt(101) ;
 //        System.out.println(num);
         int guess = -1;
+        int counter = 1;
         boolean kitalalva = false;
         Scanner sc = new Scanner(System.in);
-        while (!kitalalva)  {
+        while (!kitalalva & counter <7)  {
             guess = sc.nextInt();
             if (num == guess) {
                 System.out.println("Gratulálok!!");
                 kitalalva = true;
             } else {
-                System.out.println(num < guess ? "Kisebbre gondoltam" : "Nagyobbra gondoltam");
+                counter++;
+                System.out.println(num < guess ? "Kisebbre gondoltam " + counter : "Nagyobbra gondoltam " + counter);
             }
+        }
+        if (counter ==7) {
+            System.out.println("vesztettél :)");
         }
 
     }
