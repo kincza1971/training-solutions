35929844c92e464aa6b9787af388c3265000bb9b
Laci <kincza@gmail.com>
2020-11-25 11:30:32 +0100

    satellites

---
+++ src/main/java/immutable/satellite/Satellite.java
@@ -0,0 +1,44 @@
+package immutable.satellite;
+
+public class Satellite {
+
+    private CelestialCoordinates destinationCoordinates;
+    private String registerIdent;
+
+
+    private boolean isEmpty(String str) {
+        return (str == null || str.isBlank());
+    }
+
+    public Satellite(CelestialCoordinates destinationCoordinates, String registerIdent){
+        if (isEmpty(registerIdent)) {
+            throw new IllegalArgumentException("Register ident must not be empty!");
+        }
+        this.destinationCoordinates = destinationCoordinates;
+        this.registerIdent =registerIdent;
+    }
+
+    public void modifyDestination(CelestialCoordinates diff) {
+        destinationCoordinates = new CelestialCoordinates(
+                this.destinationCoordinates.getX()+diff.getX(),
+                this.destinationCoordinates.getY()+diff.getY(),
+                this.destinationCoordinates.getZ()+diff.getZ()
+                );
+    }
+
+    public CelestialCoordinates getDestinationCoordinates() {
+        return destinationCoordinates;
+    }
+
+    public String getRegisterIdent() {
+        return registerIdent;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s",registerIdent,destinationCoordinates.toString());
+    }
+
+
+
+}
---
+++ src/main/java/immutable/satellite/SpaceAgency.java
@@ -0,0 +1,31 @@
+package immutable.satellite;
+
+import week05d02.d03.InvalidUserException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpaceAgency {
+
+    private List<Satellite> satellites = new ArrayList<>();
+
+    public void registerSatellite(Satellite satellite) {
+        if (satellite == null){
+            throw new NullPointerException("Parameter must not be null!");
+        }
+        satellites.add(satellite);
+    }
+    public Satellite findSatelliteByRegisterIdent(String registerIdent) {
+        for (Satellite satellite : satellites) {
+            if (satellite.getRegisterIdent().equals(registerIdent)) {
+                return satellite;
+            }
+        }
+        throw new IllegalArgumentException("Satellite with the given registration cannot be found!"+registerIdent);
+    }
+
+    @Override
+    public String toString() {
+         return satellites.toString();
+    }
+}
