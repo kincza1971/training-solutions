642b21cdf914f325512a8cc7a320d65e0b195f33
Laci <kincza@gmail.com>
2020-12-08 10:25:34 +0100

    week07/d02

--- src/main/java/week07/d02/UserImpl.java
+++ src/main/java/week07/d02/UserImpl.java
@@ -26,7 +26,20 @@ public class UserImpl implements User {
         return firstname + " " + lastname;
     }
 
+    private void checkNames(String firstname, String lastname, String userName) {
+        if (firstname == null || firstname.isBlank()) {
+            throw new IllegalArgumentException( "firstname cannot be null or empty");
+        }
+        if (lastname == null || lastname.isBlank()) {
+            throw new IllegalArgumentException( "lastname cannot be null or empty");
+        }
+        if (userName == null || userName.isBlank()) {
+            throw new IllegalArgumentException( "userName cannot be null or empty");
+        }
+    }
+
     public UserImpl(String firstname, String lastname, String userName) {
+        checkNames(firstname,lastname,userName);
         this.firstname = firstname;
         this.lastname = lastname;
         this.userName = userName;
