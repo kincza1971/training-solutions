dd54d772ca3450b079b244d0c67da7cd32c8035b
Laci <kincza@gmail.com>
2020-12-08 18:29:13 +0100

    Exceptions

--- src/main/java/exceptions/faults/FaultList.java
+++ src/main/java/exceptions/faults/FaultList.java
@@ -7,8 +7,10 @@ import java.util.List;
 
 public class FaultList {
 
+    private final String NO_ERROR = ProcessingResult.NO_ERROR.getCode();
+
     private void checkList(List<String> lines) {
-        if (lines == null || lines.size() == 0){
+        if (lines == null || lines.isEmpty()){
             throw new IllegalArgumentException("processLines is null or empty");
         }
     }
@@ -38,14 +40,13 @@ public class FaultList {
     }
 
     private String processLineParts(String[] fields) {
-        if (parseDouble(fields[1]) != "NO_ERROR") {
-            if (parseDate(fields[2]) != "NO_ERROR") {
+        if (!parseDouble(fields[1]).equals(NO_ERROR)) {
+            if (!parseDate(fields[2]).equals(NO_ERROR)) {
                 return result(fields[0], ProcessingResult.VALUE_AND_DATE_ERROR.name());
-            } else {
-                return result(fields[0], ProcessingResult.VALUE_ERROR.name());
             }
+            return result(fields[0], ProcessingResult.VALUE_ERROR.name());
         }
-        if (parseDate(fields[2]) != "NO_ERROR") {
+        if (!parseDate(fields[2]).equals(NO_ERROR)) {
             return result(fields[0], ProcessingResult.DATE_ERROR.name());
         }
         return result(fields[0], ProcessingResult.NO_ERROR.name());
@@ -56,7 +57,7 @@ public class FaultList {
         if (fields.length != 3) {
             return result(fields[0], ProcessingResult.WORD_COUNT_ERROR.name());
         }
-        if (parseDouble(fields[0])!="NO_ERROR") {
+        if (!parseDouble(fields[0]).equals(NO_ERROR)) {
             return ProcessingResult.COMMENT.getCode();
         }
         return processLineParts(fields);
@@ -69,7 +70,7 @@ public class FaultList {
 
         for (String line : lines) {
             result = processOneLine(line);
-            if (!(result.contains(ProcessingResult.NO_ERROR.getCode()) || result.contains(ProcessingResult.COMMENT.getCode()))) {
+            if (!(result.contains(NO_ERROR) || result.contains(ProcessingResult.COMMENT.getCode()))) {
                 errorList.add(result);
             }
         }
