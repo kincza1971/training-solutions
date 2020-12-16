050dc34bdb0a8097adb46e240b90564d48975865
Laci <kincza@gmail.com>
2020-12-16 08:55:00 +0100

    Week08.d03

---
+++ src/main/java/week08/d03/StringList.java
@@ -0,0 +1,31 @@
+package week08.d03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringList {
+
+    private static List<String> checkParam(List <String> words) {
+        if (words == null || words.isEmpty()) {
+            throw new IllegalArgumentException("A lista nem lenet null, vagy üres");
+        }
+        return new ArrayList<>();
+    }
+
+    public static List<String> getShortestWords(List<String> words) {
+        List<String> shortestWords = checkParam(words);;
+        int minLength = Integer.MAX_VALUE;
+        int length;
+        for (String word : words) {
+            length = word.length();
+            if (length <= minLength) {
+                if (length < minLength) {
+                    minLength = length;
+                    shortestWords.clear();
+                }
+                shortestWords.add(word);
+            }
+        }
+        return new ArrayList<>(shortestWords);
+    }
+}
