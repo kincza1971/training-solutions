62a4d2e1b2e5d6c2642bf56570217f7d3d3c9a2c
Laci <kincza@gmail.com>
2021-01-08 10:20:14 +0100

    week10 d05

---
+++ src/main/java/week10/d05/Calculator.java
@@ -0,0 +1,64 @@
+package week10.d05;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void findMinMaxSum(int[] numbers) {
+        List<Integer> intList = new ArrayList<>();
+        for (Integer i : numbers) {
+            intList.add(i);
+        }
+        intList.sort(Comparator.naturalOrder());
+
+        System.out.println(getMinSum(intList));
+        System.out.println(getMaxSum(intList));
+    }
+
+    private static int getMaxSum(List<Integer> intList) {
+        int sum=0;
+        for (int i = intList.size() - 1; i > intList.size() - 5; i--) {
+            sum += intList.get(i);
+        }
+        return sum;
+    }
+
+    private static int getMinSum(List<Integer> intList) {
+        int sum =0;
+        for (int i = 0; i < 4; i++) {
+            sum += intList.get(i);
+        }
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        List<Integer> receiveNumbers = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int db;
+
+        System.out.println("Hány darab szám lesz?");
+        db = scanner.nextInt();
+        scanner.nextLine();
+
+        if (db<4) {
+            throw new IllegalArgumentException("A darabszám 4, vagy több lehet");
+        }
+
+        for (int i =0; i<db; i++) {
+            System.out.println("Kérem a számot:");
+            receiveNumbers.add(scanner.nextInt());
+        }
+        scanner.nextLine();
+
+        int[] recNums = new int[receiveNumbers.size()];
+        for (int i = 0; i<receiveNumbers.size(); i++) {
+            recNums[i] = receiveNumbers.get(i);
+        }
+
+        findMinMaxSum(recNums);
+    }
+}
