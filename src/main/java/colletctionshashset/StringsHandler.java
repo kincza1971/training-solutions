0b7a48bab353bda1a679d64c9589a0b3bc6a6d8f
Laci <kincza@gmail.com>
2021-01-17 11:59:19 +0100

    activity

--- src/main/java/colletctionshashset/StringsHandler.java
+++ src/main/java/colletctionshashset/StringsHandler.java
@@ -1,9 +1,7 @@
 package colletctionshashset;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class StringsHandler {
 
@@ -14,6 +12,40 @@ public class StringsHandler {
     public Set<String> selectIdenticalStrings(Set<String> setA, Set<String> setB) {
         setA.retainAll(setB);
         return setA;
+    }
+
+    private String randomString(int length) {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+        int code;
+        for (int i=0; i<length; i++) {
+            code = random.nextInt('z'-'a'+1) + 'a';
+            sb.append((char) code);
+        }
+        return sb.toString();
+    }
+
+    public String[] randomStringGenerator(int count, int length) {
+        String[] randomStrings = new String[count];
+        for (int i = 0; i<count; i++) {
+            randomStrings[i] = randomString(length);
+        }
+        return randomStrings;
+    }
+
+    public Set<String> filterWords(String[] rndString) {
+        Set<String> sortedUniqSet = new TreeSet<>();
+        for (String s : rndString) {
+            sortedUniqSet.add(s);
+        }
+        return sortedUniqSet;
+    }
+
+    public static void main(String[] args) {
+
+        StringsHandler sh = new StringsHandler();
+        System.out.println(sh.filterWords(sh.randomStringGenerator(30,3)));
+
     }
 
 
--- src/main/java/week11/d04/NameLength.java
+++ src/main/java/week11/d04/NameLength.java
@@ -1,19 +1,20 @@
 package week11.d04;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class NameLength {
 
-    public String getLength(List<String> strings) {
+    public List<Integer> getLength(List<String> strings) {
         Set<Integer> result = new TreeSet<>();
          for (String s : strings) {
              if (s.startsWith("J")) {
                  result.add(s.length());
              }
          }
-         return result.toString();
+         return new ArrayList<>(result);
     }
 
     public static void main(String[] args) {
