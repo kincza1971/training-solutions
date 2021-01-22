7d8619a8daba5926832844d327c24862bfe80492
Laci <kincza@gmail.com>
2021-01-22 10:59:36 +0100

    refactor/packagename

--- src/main/java/stringbuilder/PalindromeValidator.java
+++ src/main/java/stringbuilder/PalindromeValidator.java
@@ -2,13 +2,14 @@ package stringbuilder;
 
 public class PalindromeValidator {
     public boolean isPalindrome(String word) {
-        StringBuilder sb = new StringBuilder(word.toUpperCase());
-        return sb.equals(sb.reverse());
+        StringBuilder sb = new StringBuilder(word.trim().toUpperCase());
+        return sb.toString().equals(sb.reverse().toString());
     }
 
 
     public static void main(String[] args) {
         PalindromeValidator pv = new PalindromeValidator();
+        System.out.println(pv.isPalindrome("zörög"));
         System.out.println(pv.isPalindrome("Görög"));
     }
 }
