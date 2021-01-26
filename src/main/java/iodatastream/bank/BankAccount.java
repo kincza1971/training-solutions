9d6669825a6a22aca084eb19f04db87e438617c4
Laci <kincza@gmail.com>
2021-01-26 15:14:20 +0100

    packages/refactor names

---
+++ src/main/java/iodatastream/bank/BankAccount.java
@@ -0,0 +1,26 @@
+package iodatastream.bank;
+
+
+public class BankAccount {
+    private final String accountNumber;
+    private final String owner;
+    private int balance;
+
+    public BankAccount(String accountNumber, String owner, int balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
---
+++ src/main/java/iodatastream/bank/BankAccountManager.java
@@ -0,0 +1,30 @@
+package iodatastream.bank;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class BankAccountManager {
+
+    public BankAccount readAccoun(InputStream is) {
+//        Path file = Path.of("/src/main/java/resources/ioreadstream/bankaccounts.txt");
+        try (DataInputStream dis = new DataInputStream(new BufferedInputStream(is)) ){
+            return new BankAccount(dis.readUTF(),dis.readUTF(),dis.readInt());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void saveAccount(Path file, BankAccount ba) {
+        try (DataOutputStream os = new DataOutputStream(new BufferedOutputStream(Files.newOutputStream(file)))) {
+            os.writeUTF(ba.getAccountNumber());
+            os.writeUTF(ba.getOwner());
+            os.writeInt(ba.getBalance());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
