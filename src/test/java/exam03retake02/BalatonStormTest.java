18f21cf752ec233e3069e6ec161cde1e59a8d5bd
Laci <kincza@gmail.com>
2021-02-26 17:04:40 +0100

    exam03retake02/tests

---
+++ src/test/java/exam03retake02/BalatonStormTest.java
@@ -0,0 +1,11 @@
+package exam03retake02;
+
+class BalatonStormTest {
+
+//    @Test
+//    void testGetStationsInStorm() throws IOException {
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(BalatonStormTest.class.getResourceAsStream("storm.json")))) {
+//            assertEquals(List.of("Ábrahámhegy", "Balatonfüred", "Balatonszárszó"), new BalatonStorm().getStationsInStorm(reader));
+//        }
+//    }
+}
